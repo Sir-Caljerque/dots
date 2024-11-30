1718928d09b1ad5a1140e0b4d5dcbb227c8b06b8
Sir-Caljerque <getinihavecandy2@proton.me>
2024-11-30 04:04:28 -0500

    stuff

--- certs/Certs/Security+/2 - Threats, Vulnerabilities, and Mitigations.md
+++ certs/Certs/Security+/2 - Threats, Vulnerabilities, and Mitigations.md
@@ -170,37 +170,6 @@ Dec 2020
 Attackers gained access to development system
 Added their own malicious codes to the update
 
-# --Study areas--
-
-## 5.2
-Q: Which of the following describers a monetary loss if one event occurs
-A. **ALE** <u>Annual loss expectancy</u> - How much money you lose in a span of 12 months
-B. **SLE** <u>single loss expectancy</u> - money loss if one event occurs, like a server going down
-C. **RTO** <u>Recovry time objective</u> - how fast you can get everything ready if event occurs ... How long it takes to get service up
-    **PO** <u>Recovery point objective</u> - How much tolerance you have / how much data you're willing to lose until action needs to be taken
-D. **ARO** <u>annualized rate of occurance</u> - event frequency / how many times a server crashes in a span of time
-extra: MTTR Mean time to repair 
-![[Pasted image 20241108203712.png]]
-
-## Study port numbers
-
-- 20, 21
-- 22
-- 23
-- 25
-- 53
-- 67/68
-- 80/443
-- 110
-- 143
-- 161
-- 389
-- 445
-- 514
-- 1433
-- 3389
-
-## IT structure
 
 ### Escalation
 
@@ -209,3 +178,7 @@ does not necessarily mean privilege escalation
 for example, you make a support ticket because your OS crashes,
 it goes to L1s, who *dont know* how to solve it, so they **escalate** to L2s
 L2s dont have the privileges, so they **escalate** to the next group
+
+# Virtualization vulnerabilities
+
+When an attacker can access resources of one VM from another, its called a *VM **Escape***
--- nvim/dot-config/nvim/lua/plugins/lspsaga.lua
+++ nvim/dot-config/nvim/lua/plugins/lspsaga.lua
@@ -1,80 +1,80 @@
 return {
-  "glepnir/lspsaga.nvim",
-  event = "LspAttach",
-  config = function()
-    require("lspsaga").setup({
-      -- keybinds for navigation in lspsaga window
-      symbol_in_winbar = {
-        enable = true,
-        separator = " › ",
-        hide_keyword = false, -- when true some symbols like if and for
-        -- Will be ignored (need treesitter)
-        show_file = true,     --Show file name before symbols
-        folder_level = 1,     --Show how many folder layers before the file name
-        color_mode = true,    --true mean the symbol name and icon have same color. Otherwise, symbol name is light-white
-        delay = 300,          --Dynamic render delay
-      },
-      code_action = {
-        num_shortcut = true,
-        show_server_name = true,
-        extend_gitsigns = false,
-        keys = {
-          quit = "q",
-          exec = "<CR>",
-        },
-      },
+    "glepnir/lspsaga.nvim",
+    event = "LspAttach",
+    config = function()
+        require("lspsaga").setup({
+            -- keybinds for navigation in lspsaga window
+            symbol_in_winbar = {
+                enable = true,
+                separator = " › ",
+                hide_keyword = false, -- when true some symbols like if and for
+                -- Will be ignored (need treesitter)
+                show_file = true, --Show file name before symbols
+                folder_level = 1, --Show how many folder layers before the file name
+                color_mode = true, --true mean the symbol name and icon have same color. Otherwise, symbol name is light-white
+                delay = 300,  --Dynamic render delay
+            },
+            code_action = {
+                num_shortcut = true,
+                show_server_name = true,
+                extend_gitsigns = false,
+                keys = {
+                    quit = "q",
+                    exec = "<CR>",
+                },
+            },
 
-      move_in_saga = { prev = "<C-p>", next = "<C-n>" },
-      -- use enter to open file with finder
-      finder_action_keys = {
-        open = "<CR>",
-      },
-      -- use enter to open file with definition preview
-      definition = {
-        keys = {
-          edit = "<C-c>o",
-          vsplit = "<C-c>v",
-          split = "<C-c>h",
-          quit = "q",
-          close = "<C-c>k",
-        },
-      },
-      diagnostic = {
-        extend_relatedInformation = true,
-      },
-      finder = {
-        default = "def+tyd+ref+imp",
-        keys = {
-          shuttle = "[w",       --shuttle bettween the finder layout window
-          toggle_or_open = "o", -- toggle expand or open
-          vsplit = "s",         -- open in vsplit
-          split = "i",          -- open in split
-          tabe = "t",           -- open in tabe
-          tabnew = "r",         -- open in new tab
-          quit = "q",           -- quit the finder, only works in layout left window
-          close = "<C-c>k",     -- close finder
-        },
-      },
-      outline = {
-        win_position = "right",
-        auto_close = true,
-      },
-      lightbulb = {
-        enable = false,
-      },
-      keys = {
-        toggle_or_jump = "<A-o>",
-        quit = "<A-O>",
-        -- jump = '', -- idk what to set this to
-      },
-      rename = {
-        in_select = false,
-        auto_save = true,
-      },
-    })
-  end,
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
-  },
+            move_in_saga = { prev = "<C-p>", next = "<C-n>" },
+            -- use enter to open file with finder
+            finder_action_keys = {
+                open = "<CR>",
+            },
+            -- use enter to open file with definition preview
+            definition = {
+                keys = {
+                    edit = "<C-c>o",
+                    vsplit = "<C-c>v",
+                    split = "<C-c>h",
+                    quit = "q",
+                    close = "<C-c>k",
+                },
+            },
+            diagnostic = {
+                extend_relatedInformation = true,
+            },
+            finder = {
+                default = "def+tyd+ref+imp",
+                keys = {
+                    shuttle = "[w", --shuttle bettween the finder layout window
+                    toggle_or_open = "o", -- toggle expand or open
+                    vsplit = "s", -- open in vsplit
+                    split = "i", -- open in split
+                    tabe = "t", -- open in tabe
+                    tabnew = "r", -- open in new tab
+                    quit = "q", -- quit the finder, only works in layout left window
+                    close = "<C-c>k", -- close finder
+                },
+            },
+            outline = {
+                win_position = "left",
+                auto_close = true,
+            },
+            lightbulb = {
+                enable = false,
+            },
+            keys = {
+                toggle_or_jump = "<A-o>",
+                quit = "<A-O>",
+                -- jump = '', -- idk what to set this to
+            },
+            rename = {
+                in_select = false,
+                auto_save = true,
+            },
+        })
+    end,
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
 }
